26b2dd5b237feab29e7d21ebb34408caa15b023c
leon0x01 <ushrestha358@gmail.com>
2024-01-10 12:52:06 +0545

    chore: updated new packageInfo

--- scripts/utils/packageInfo.ts
+++ scripts/utils/packageInfo.ts
@@ -1,4 +1,4 @@
-export let packageId = '0x233605ab69ad1682eed970e9a0b65030328da16feb868adcf180e5030477dccb';
-export let GameListId =  '0xcecc9ea9b754895516a647b92f9bccb481724ec010f0737214323e777aa5b300';
-export let RPSCapId =  '0x2b8ee5c6fcbb4309b67c58e6ea303c6ddcce233648698fdbcfe1a0f71d20d39c';
-export let RPSId = '0x4ae52208823cc750e0bf3d9ffce8e652e97975424dcd54b2e8bd3e9e0766891c';
+export let packageId = '0x3ed13fc8ead67eb01be1dc281eec82b34ab542da0e274b0dc71b5c75584e7435';
+export let GameListId =  '0xaf921e3799cca13f2f0e8bbdc9f6070759b5de53b3d369b0a17cd7ef237669c5';
+export let RPSCapId =  '0xd351e4ebf1e06844f8091283f88ae0cffc01bcdcf4cf41ffd4148e28a471cc0d';
+export let RPSId = '0x94ad46ef2d4b387d6dcc0074720f3f1511f2e09acefd483ab3f2043535aa5176';
