3264cad4fafa6502622eac53e48af0e363065d6c
spo0ds <asmitadhikari540@gmail.com>
2024-01-12 13:28:08 +0545

    feat: script to cancel game

---
+++ scripts/src/cancel_game.ts
@@ -0,0 +1,24 @@
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import * as dotenv from 'dotenv';
+import getExecStuff from '../utils/execstuff';
+import { packageId, RPSId, GameListId } from '../utils/packageInfo';
+dotenv.config();
+
+async function play_rps_game() {
+    const { keypair, client } = getExecStuff();
+    const tx = new TransactionBlock();
+    tx.moveCall({
+        target: `${packageId}::rps::cancel_game`,
+        arguments: [
+            tx.object(GameListId),
+            tx.pure.address(RPSId),
+        ]
+    });
+    const result = await client.signAndExecuteTransactionBlock({
+        signer: keypair,
+        transactionBlock: tx,
+    });
+    console.log({ result });
+}
+
+play_rps_game();
