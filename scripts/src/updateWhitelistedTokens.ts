c65166feaba0d7a9434ca5c3119941c67fe692b2
spo0ds <asmitadhikari540@gmail.com>
2024-01-19 14:17:27 +0545

    feat: script to whitelist token

---
+++ scripts/src/updateWhitelistedTokens.ts
@@ -0,0 +1,28 @@
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import * as dotenv from 'dotenv';
+import getExecStuff from '../utils/execstuff';
+import { packageId, RPSCapId, WhiteListedTokensId, CoinMetadataId } from '../utils/packageInfo';
+dotenv.config();
+
+async function updateToken() {
+    const { keypair, client } = getExecStuff();
+    const tx = new TransactionBlock();
+    tx.moveCall({
+        target: `${packageId}::rps::update_whitelist_token`,
+        arguments: [
+            tx.object(RPSCapId),
+            tx.object(WhiteListedTokensId)
+        ],
+        // typeArguments: [`${packageId}::rps::RPS`],
+        typeArguments: ["0x2::sui::SUI"],
+    });
+
+    const result = await client.signAndExecuteTransactionBlock({
+        signer: keypair,
+        transactionBlock: tx,
+    });
+
+    console.log({ result });
+}
+
+updateToken();
